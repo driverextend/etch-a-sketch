eacde0ab702f58d8f757e709cad1391f1f1b4235
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-07-31 16:02:59 -0500

    Allow user to change the layout with input
    There was a bug where the previous drawable divs
    would not get removed before the new ones, which
    was fixed by me.

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const container = document.getElementById("container")
+const button = document.querySelector("button")
 
-let sideLength = parseInt(getComputedStyle(container).width)
+let containerSize = parseInt(getComputedStyle(container).width)
 
-function calculateWidthAndHeight(amount = 16){
-    return `${sideLength / amount}px`
+function renderGridBySquaresPerSide(amount = 16){
+    if (amount > 100) amount = 100
+
+    clearGrid()
+    
+    for (let count = 1; count <= amount * amount; count++){
+        const div = document.createElement('div')
+    
+        let sideLength = `${containerSize/amount}px` 
+        div.style.width = sideLength
+        div.style.height = sideLength
+    
+        div.addEventListener("mouseover", () => {
+            div.classList.add("change-to-black")
+        })
+    
+        container.appendChild(div)
+    }
 }
 
-for (let count = 1; count <= 256; count++){
-    const div = document.createElement('div')
-
-    div.style.width = calculateWidthAndHeight()
-    div.style.height = calculateWidthAndHeight()
-
-    div.addEventListener("mouseover", () => {
-        div.classList.add("change-to-black")
-    })
-
-
-    container.appendChild(div)
-
+function clearGrid(){
+    container.innerHTML = ""
 }
+
+button.addEventListener("click", () => {
+    let userInput = parseInt(prompt(`new number of squares per side:\n(default value: 16)`))
+
+    renderGridBySquaresPerSide(userInput)
+
+    button.innerHTML = `number of squares per side: ${userInput} x ${userInput}`
+})
+
+renderGridBySquaresPerSide()
 
